c1c32b4274ce3e9cc17e981e55ad7947991cb303
ArtemY <35269498+JavaScriptProger@users.noreply.github.com>
2018-06-20 16:08:46 +0300

    Add files via upload

---
+++ Ping-Pong/MainForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Artem
+ * Date: 20.06.2018
+ * Time: 14:06
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Ping_Pong
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(239, 169);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(557, 73);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Game Over!";
+			this.label1.Visible = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(862, 433);
+			this.Controls.Add(this.label1);
+			this.MaximumSize = new System.Drawing.Size(880, 480);
+			this.MinimumSize = new System.Drawing.Size(880, 480);
+			this.Name = "MainForm";
+			this.Text = "Ping-Pong";
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainFormPaint);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainFormKeyDown);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ Ping-Pong/MainForm.cs
@@ -0,0 +1,108 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Artem
+ * Date: 20.06.2018
+ * Time: 14:06
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Ping_Pong
+{
+	public partial class MainForm : Form
+	{
+		Graphics graphics;
+		Timer timer = new Timer();
+		
+		int FPS = 60;
+		int player1y;
+		
+		int ballx;
+		int bally;
+		int ballspdx = 3;
+		int ballspdy = 3;
+		
+		//-------------------------------	
+		public MainForm()
+		{
+			InitializeComponent();
+			
+			timer.Enabled = true;
+        	timer.Interval = 1000 / FPS;
+        	timer.Tick += new EventHandler(TimerCallback);
+        	
+        	ballx = this.Width / 2 - 10;
+        	bally = this.Height / 2 - 10;
+		}
+		
+		//Graphics
+		void DrawRectangle(int x, int y, int w, int h, SolidBrush Color){
+			graphics.FillRectangle(Color, new Rectangle(x,y,w,h));
+		}
+		
+		void UpdateBall(){
+			ballx += ballspdx;
+			bally += ballspdy;
+			
+			if(ballx + 40 > this.Width){
+				ballspdx = -ballspdx;
+			}
+
+			if(bally < 0 || bally + 40 > this.Height){
+				ballspdy = -ballspdy;
+			}
+			
+			if(IsCollided()){
+				ballspdx = -ballspdx;
+			}
+			
+			if(ballx < 0){
+				label1.Visible = true;
+				timer.Stop();
+			}
+		}
+		
+		bool IsCollided(){
+			if(ballx < 20 && bally > player1y && bally < player1y + 130){
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		void TimerCallback(object sender, EventArgs e)
+	    {
+			DrawRectangle(0,player1y,20,130,new SolidBrush(Color.Black));
+			DrawRectangle(ballx,bally,20,20,new SolidBrush(Color.Black));
+			UpdateBall();
+			
+	        this.Invalidate();
+	        return;
+	    }
+		
+		//Control
+		void MainFormPaint(object sender, PaintEventArgs e)
+		{
+			graphics =  CreateGraphics();
+			DrawRectangle(0,player1y,20,130,new SolidBrush(Color.Black));
+			DrawRectangle(ballx,bally,20,20,new SolidBrush(Color.Black));
+		}
+		
+		void MainFormKeyDown(object sender, KeyEventArgs e)
+		{
+			//MessageBox.Show(Convert.ToString(e.KeyValue));
+			int key = e.KeyValue;
+			if(key == 38){
+				player1y -= 5;
+			}
+
+			if(key == 40){
+				player1y += 5;
+			}
+		}
+	}
+}
